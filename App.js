c2e5ed024b72c926f1b1181a0c71c48383f29b62
maydayo <wanwisa.silanan@gmail.com>
2019-06-17 21:27:41 +0700

    apollo init

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
-import React, {Component} from 'react';
-import Routes from "./Routes"
+import React, { Component } from "react";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createUploadLink } from "apollo-upload-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import Routes from "./Routes";
 export default class App extends Component {
+  constructor(...args) {
+    super(args);
+    const httpLink = new createUploadLink({
+      uri: "http://192.168.2.95:3005/graphql/",
+    });
+
+    this.client = new ApolloClient({
+      link: httpLink,
+      cache: new InMemoryCache()
+    });
+  }
+
   render() {
     return (
-      <Routes />
+      <ApolloProvider client={this.client}>
+        <Routes />
+      </ApolloProvider>
     );
   }
 }
