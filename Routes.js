0e1d7b1e00a673c1125126f3dd686bfec38c54aa
maydayo <wanwisa.silanan@gmail.com>
2019-06-19 13:33:50 +0700

    remove header

--- Routes.js
+++ Routes.js
@@ -3,13 +3,18 @@ import { View, Text } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { TakePicScreen, ReviewPicScreen } from "./src/screens";
 
-const AppStackNavigator = createStackNavigator({
-  TakePic: {
-    screen: TakePicScreen
-  },
-  ReviewPic: {
+const AppStackNavigator = createStackNavigator(
+  {
+    TakePic: {
+      screen: TakePicScreen
+    },
+    ReviewPic: {
       screen: ReviewPicScreen
+    }
+  },
+  {
+    headerMode: "none"
   }
-});
+);
 
 export default createAppContainer(AppStackNavigator);
